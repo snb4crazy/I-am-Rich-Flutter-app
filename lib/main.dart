50e4a0f186986e9b4623fc6ed03f1b2f0502fa9c
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-02 01:07:39 -0400

    Add Body: Network Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("I Am Rich"),
-        backgroundColor: Colors.blueGrey[900],
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://proserge.kh.ua/wp-content/uploads/2020/08/Finish-race-1024x683.jpg'),
+          ),
+        ),
       ),
-    )),
+    ),
   );
 }
