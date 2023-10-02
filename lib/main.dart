0b3fe8eb4bbb2b643245b034070ccc23872f15e5
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-02 00:21:43 -0400

    Refactoring Centered Hello World Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text('Hello world'))));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello world'),
+      ),
+    ),
+  );
+}
