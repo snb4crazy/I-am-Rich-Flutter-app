c176949d6e682c42a54804b109163e7653312f5f
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-02 00:38:16 -0400

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
       ),
-    ),
+    )),
   );
 }
