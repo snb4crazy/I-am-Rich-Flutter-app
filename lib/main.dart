282364ddfd2583577b19746395a25e1a080c513c
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-07 15:40:04 -0400

    Display asset image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//visit https://www.appicon.co/ for generating icons from an image
 void main() {
   runApp(
     MaterialApp(
@@ -11,8 +12,8 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://proserge.kh.ua/wp-content/uploads/2020/08/Finish-race-1024x683.jpg'),
+            //image: NetworkImage('https://proserge.kh.ua/wp-content/uploads/2020/08/Finish-race-1024x683.jpg'),
+            image: AssetImage('images/niagara.jpeg'),
           ),
         ),
       ),
